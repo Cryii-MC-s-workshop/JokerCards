12b9f7b25dd1d2e975883ab127f966205f7b0404
spozap <spozapdev@gmail.com>
2025-06-07 19:18:28 +0200

    initial commit

---
+++ src/main/kotlin/dev/spozap/JokerCards.kt
@@ -0,0 +1,14 @@
+package dev.spozap
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class JokerCards : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
